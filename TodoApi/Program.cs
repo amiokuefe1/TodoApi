d2edaa566c8d3eb07fb1393190d73ffe6d8e057e
amiokuefe1 <amiokuefe@gmail.com>
2022-02-09 16:06:02 +0100

    I'm about to rewrite the API starting from the program.cs file

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,7 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World");
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+        await db.Todos.ToListAsync());
 
 app.Run();
